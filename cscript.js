62382577984f0a1b7c248fafdabbbf20d9142fcf
sruthiundamatla <131868018+sruthiundamatla@users.noreply.github.com>
2023-05-02 11:19:48 +0530

    Add files via upload

---
+++ cscript.js
@@ -0,0 +1,17 @@
+let result = document.getElementById("result");
+
+function addToResult(value) {
+	result.value += value;
+}
+
+function clearResult() {
+	result.value = "";
+}
+
+function backspace() {
+	result.value = result.value.slice(0, -1);
+}
+
+function calculate() {
+	result.value = eval(result.value);
+}
